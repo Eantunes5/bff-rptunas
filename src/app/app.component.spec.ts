ba45b5b6da33dd5e381f160dbab6338af9e4cf44
Eduardo Siegert Antunes <eantunes5@hotmail.com>
2026-03-16 21:45:22 -0300

    Correeções de layout

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'rptunas' title`, () => {
+  it(`should have the 'RPTunas' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rptunas');
+    expect(app.title).toEqual('RPTunas');
   });
 
   it('should render title', () => {
